4ff9f266626be7a14d34b670ac4fa74a52fba7c5
ES-Alexander <sandman.esalexander@gmail.com>
2026-03-18 06:56:08 +0800

    libraries: AP_Motors6DOF: fix remaining OMNI6 cross-axis coupling

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -178,12 +178,12 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
     case SUB_FRAME_CUSTOM:
         // Put your custom motor setup here
         _frame_class_string = "OMNI6";
-        add_motor_raw_6dof(AP_MOTORS_MOT_1,      1,     -1,      0,     -1,      0,      1,     1);
-        add_motor_raw_6dof(AP_MOTORS_MOT_2,     -1,     -1,      0,     -1,      0,     -1,     2);
-        add_motor_raw_6dof(AP_MOTORS_MOT_3,      1,      1,      0,     -1,      0,     -1,     3);
-        add_motor_raw_6dof(AP_MOTORS_MOT_4,     -1,      1,      0,     -1,      0,      1,     4);
-        add_motor_raw_6dof(AP_MOTORS_MOT_5,   -0.5,      0,     -1,      0,      1,      0,     5);
-        add_motor_raw_6dof(AP_MOTORS_MOT_6,    0.5,      0,      1,      0,      1,      0,     6);
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,      1,     -1,      0,     -1,     0.1,      1,     1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,     -1,     -1,      0,     -1,     0.1,     -1,     2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,      1,      1,      0,     -1,    -0.1,     -1,     3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,     -1,      1,      0,     -1,    -0.1,      1,     4);
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,      1,      0,     -1,      0,       1,      0,     5);
+        add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1,      0,      1,      0,       1,      0,     6);
         break;
 
     case SUB_FRAME_SIMPLEROV_3:
